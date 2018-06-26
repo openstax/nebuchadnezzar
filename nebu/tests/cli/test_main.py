fa72344a38867f731ef208a8b651127bce90519b
tomjw64 <tom.jw64@gmail.com>
2018-06-26 11:55:02 -0500

    Add test for latest version suggestion

--- nebu/tests/cli/test_main.py
+++ nebu/tests/cli/test_main.py
@@ -13,3 +13,22 @@ def test_for_version(monkeypatch, invoker):
 
     expected_output = 'Nebuchadnezzar {}\n'.format(version)
     assert result.output == expected_output
+
+
+def test_old_version(monkeypatch, invoker):
+    version = '0.0.0'
+
+    monkeypatch.setattr(nebu.cli.main, '__version__', version)
+
+    from nebu.cli.main import cli
+    args = ['--version']
+    result = invoker(cli, args)
+    assert result.exit_code == 0
+
+    import re
+    expected_output = 'Nebuchadnezzar {}\n'.format(version)
+    expected_output += "Version available for install.\n"
+    output_no_version = re.sub(r"Version \w+(\.\w+)* available",
+                               "Version available",
+                               result.output)
+    assert output_no_version == expected_output
