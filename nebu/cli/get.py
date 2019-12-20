8e799a911f3405b9731676a4943389fe0ebe56fa
Brenda Nguyen <bn16@rice.edu>
2019-12-20 17:10:43 -0600

    refactor separate concerns for get continued

--- nebu/cli/get.py
+++ nebu/cli/get.py
@@ -17,128 +17,9 @@ from .exceptions import (MissingContent,
 import concurrent.futures
 import urllib.request
 
-@click.command()
-@common_params
-@click.option('-d', '--output-dir', type=click.Path(),
-              help="output directory name (can't previously exist)")
-@click.option('-t', '--book-tree', is_flag=True,
-              help="create human-friendly book-tree")
-@click.option('-r', '--with-resources', is_flag=True, default=False,
-              help="Also get all resources (images)")
-@click.argument('env')
-@click.argument('col_id')
-@click.argument('col_version')
-@click.pass_context
-def get(ctx, env, col_id, col_version, output_dir, book_tree, with_resources):
-    """download and expand the completezip to the current working directory"""
-    start_time = time.time()
-    base_url = build_archive_url(ctx, env)
-
-    version = None
-    req_version = col_version
-    if col_version.count('.') > 1:
-        full_version = col_version.split('.')
-        col_version = '.'.join(full_version[:2])
-        version = '.'.join(full_version[1:])
-
-    col_hash = '{}/{}'.format(col_id, col_version)
-    # Fetch metadata
-    url = '{}/content/{}'.format(base_url, col_hash)
-
-    # Create a request session with retries if there's failed DNS lookups,
-    # socket connections and connection timeouts.
-    # See https://stackoverflow.com/questions/33895739/
-    session = requests.Session()
-    adapter = requests.adapters.HTTPAdapter(max_retries=5)
-    session.mount('https://', adapter)
-
-    # Request the collection's metadata by requests the legacy url,
-    # which is redirected to the metadata url.
-    resp = session.get(url)
-    if resp.status_code >= 400:
-        raise MissingContent(col_id, req_version)
-    col_metadata = resp.json()
-
-    # If the response is a collated (aka baked) version of the book,
-    # request the non-collated (aka raw) version instead.
-    if col_metadata['collated']:
-        url = resp.url + '?as_collated=False'
-        resp = session.get(url)
-        if resp.status_code >= 400:
-            # This should never happen - indicates that only baked exists?
-            raise MissingContent(col_id, req_version)
-        col_metadata = resp.json()
-
-    uuid = col_metadata['id']
-    # metadata fetch used legacy IDs, so will only have
-    # the latest minor version - if "version" is set, the
-    # user requested an explicit minor (3 part version: 1.X.Y)
-    # refetch metadata, using uuid and requested version
-    if version and version != col_metadata['version']:
-        url = '{}/contents/{}@{}'.format(base_url, uuid, version) + \
-              '?as_collated=False'
-        resp = session.get(url)
-        if resp.status_code >= 400:  # Requested version doesn't exist
-            raise MissingContent(col_id, req_version)
-        col_metadata = resp.json()
-
-    version = col_metadata['version']
-
-    # Generate full output dir as soon as we have the version
-    if output_dir is None:
-        output_dir = Path.cwd() / '{}_1.{}'.format(col_id, version)
-    else:
-        output_dir = Path(output_dir)
-        if not output_dir.is_absolute():
-            output_dir = Path.cwd() / output_dir
-
-    # ... and check if it's already been downloaded
-    if output_dir.exists():
-        raise ExistingOutputDir(output_dir)
-
-    # Fetch extras (includes head and downloadable file info)
-    url = '{}/extras/{}@{}'.format(base_url, uuid, version)
-    resp = session.get(url)
-
-    # Latest defaults to successfully baked - we need headVersion
-    if col_version == 'latest':
-        version = resp.json()['headVersion']
-        url = '{}/extras/{}@{}'.format(base_url, uuid, version)
-        resp = session.get(url)
-
-    col_extras = resp.json()
-
-    if version != col_extras['headVersion']:
-        logger.warning("Fetching non-head version of {}."
-                       "\n    Head: {},"
-                       " requested {}".format(col_id,
-                                              col_extras['headVersion'],
-                                              version))
-        if not(confirm("Fetch anyway? [y/n] ")):
-            raise OldContent()
-
-    # Write tree
-    tree = col_metadata['tree']
-    os.mkdir(str(output_dir))
-
-    num_pages = _count_leaves(tree) + 1  # Num. of xml files to fetch
-    try:
-        label = 'Getting {}'.format(output_dir.relative_to(Path.cwd()))
-    except ValueError:
-        # Raised ONLY when output_dir is not a childnode of cwd
-        label = 'Getting {}'.format(output_dir)
-    with click.progressbar(length=num_pages,
-                           label=label,
-                           width=0,
-                           show_pos=True) as pbar:
-        _write_node(tree, base_url, output_dir, book_tree, with_resources, pbar)
-
-    duration = time.time() - start_time
-    print("Fetch took {} seconds".format(duration))
-
-def _count_leaves(node):
+def _count_nodes(node):
     if 'contents' in node:
-        return sum([_count_leaves(childnode) for childnode in node['contents']])
+        return sum([_count_nodes(childnode) for childnode in node['contents']])
     else:
         return 1
 
@@ -163,51 +44,27 @@ def store_sha1(sha1, write_dir, filename):
     with (write_dir / '.sha1sum').open('a') as s:
         s.write('{}  {}\n'.format(sha1, filename))
 
-# Retrieve a single page and report the URL and contents
-def load_url(url, timeout):
+def _load_url(url, timeout):
     with urllib.request.urlopen(url, timeout=timeout) as conn:
         return conn.read()
 
-def _get_resources(resources, filename, write_dir):
+def get_resources(resources, filename, write_dir):
     print('\n++++++++++++ GETTING RESOURCES +++++++++++')
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         futures_to_url =  {}
         for resource in resources:
             url = resources[resource]['url']
-            futures_to_url.update({executor.submit(load_url, url, 60): resource})
+            futures_to_url.update({executor.submit(_load_url, url, 60): resource})
 
         for future in concurrent.futures.as_completed(futures_to_url):
             resource = futures_to_url[future]
-            if resource == filename:
-                print('NOT GETTING RESOURCE FOR: {}'.format(resource))
-                pass
-            else:
+            if not resources[resource]['skip_download']:
                 content = future.result()
                 file_path = write_dir / resource
                 file_path.write_bytes(content)
-                store_sha1(resources[resource]['id'], write_dir, resource) # NOTE: the id is the sha1
+                # NOTE: 'id' is the sha1
+                store_sha1(resources[resource]['id'], write_dir, resource)
                 print('GETTING RESOURCE FOR: {}'.format(resource))
-
-
-# def _get_resources(resources, filename, write_dir):
-#     print('\n++++++++++++ GETTING RESOURCES +++++++++++')
-#     for res in resources:  # Dict keyed by resource filename
-#         #  Exclude core file, already written out
-#         if res != filename:
-#             filepath = write_dir / res
-#             url = resources[res]['url']
-#             file_resp = requests.get(url)
-#
-#             hey = "check file resp"
-#             from IPython import embed
-#             embed()
-#
-#             filepath.write_bytes(file_resp.content)
-#             store_sha1(resources[res]['id'], write_dir, res) # NOTE: the id is the sha1
-#             print('GETTING RESOURCE FOR: {}'.format(res))
-#         else:
-#             print('NOT GETTING RESOURCE FOR: {}'.format(res))
 
 def _clean_resources(metadata, base_url):
     resources = {}
@@ -218,10 +75,9 @@ def _clean_resources(metadata, base_url):
     So they are excluded from resources to download.
     """
     for res in metadata['resources']:
-        if res['filename'] != file_to_skip:
-            res.update({'url': '{}/resources/{}'.format(base_url, res['id'])})
-            resources.update({res['filename'] : res})
-
+        res.update({'url': '{}/resources/{}'.format(base_url, res['id'])})
+        res.update({'skip_download': res['filename'] == file_to_skip})
+        resources.update({res['filename'] : res})
     return resources
 
 def _cache_node_sha1(write_dir, filename):
@@ -234,11 +90,9 @@ def _make_human_readable_directories(out_dir, book_level, position, node):
         dirname = _safe_name(node['title'])
     else:
         dirname = '{:02d} {}'.format(position[book_level], _safe_name(node['title']))
-
     out_dir = out_dir / dirname
     os.mkdir(str(out_dir))
     return out_dir
-
 
 def _write_node(node, base_url, out_dir, book_tree=False, with_resources=False,
                 pbar=None, depth=None, position={0: 0}, book_level=0):
@@ -255,7 +109,7 @@ def _write_node(node, base_url, out_dir, book_tree=False, with_resources=False,
     print("pbar: {}".format(pbar))
     print("depth: {}".format(depth))                      ## Depth is height of tree
     print("position: {}".format(position))                ## Used to reset the lowest book_level counter (pages) per chapter, i.e. position: {book: ##, chapter: ##, page: ##}
-    print("book_level: {}".format(book_level))                      ## Levels - 0: book, 1: unit , 2: chapter, 3: module/page, .... for A & P book col11496
+    print("book_level: {}".format(book_level))            ## Book Levels - 0: book, 1: unit , 2: chapter, 3: module/page, .... for A & P book col11496
     """ Remaining args are used for recursion """
 
     if depth is None:
@@ -291,7 +145,7 @@ def _write_node(node, base_url, out_dir, book_tree=False, with_resources=False,
         _cache_node_sha1(write_dir, filename)
 
         if with_resources:
-            _get_resources(resources, filename, write_dir)
+            get_resources(resources, filename, write_dir)
 
         if pbar is not None:
             pbar.update(1)
@@ -315,3 +169,132 @@ def _write_node(node, base_url, out_dir, book_tree=False, with_resources=False,
             #                 pbar, depth, position, book_level)
             _write_node(childnode, base_url, out_dir, book_tree, with_resources,
                         pbar, depth, position, book_level)
+
+def _get_versions(col_version):
+    version = None
+    req_version = col_version
+    if col_version.count('.') > 1:
+        full_version = col_version.split('.')
+        col_version = '.'.join(full_version[:2])
+        version = '.'.join(full_version[1:])
+    return req_version, col_version, version
+
+def fetch_metadata(session, base_url, legacy_url, col_id, req_version, version):
+    # Legacy url redirects to the metadata url
+    resp = session.get(legacy_url)
+    if resp.status_code >= 400:
+        raise MissingContent(col_id, req_version)
+    collection_metadata = resp.json()
+
+    baked_version = collection_metadata['collated']
+    if baked_version:
+        raw_url = resp.url + '?as_collated=False'
+        raw_version = session.get(raw_url)
+        if raw_version.status_code >= 400:
+            # Should never happen: Indicates that only baked exists.
+            raise MissingContent(col_id, req_version)
+        collection_metadata = raw_version.json()
+
+    uuid = collection_metadata['id']
+    if version and version != collection_metadata['version']:
+        # If "version" is set and explicit minor (3 part version: 1.X.Y) was requested
+        # Refetch metadata, using uuid and requested version
+        raw_query = 'as_collated=False'
+        url = '{}/contents/{}@{}?{}'.format(base_url, uuid, version, raw_query)
+        resp = session.get(url)
+        if resp.status_code >= 400:
+            raise MissingContent(col_id, req_version)
+        collection_metadata = resp.json()
+    return collection_metadata
+
+def build_legacy_url(base_url, col_id, col_version):
+    col_hash = '{}/{}'.format(col_id, col_version)
+    legacy_url = '{}/content/{}'.format(base_url, col_hash)
+    return legacy_url
+
+def build_output_directory(output_dir, col_id, version):
+    if output_dir is None:
+        output_dir = Path.cwd() / '{}_1.{}'.format(col_id, version)
+    else:
+        output_dir = Path(output_dir)
+        if not output_dir.is_absolute():
+            output_dir = Path.cwd() / output_dir
+    if output_dir.exists():
+        raise ExistingOutputDir(output_dir)
+    return output_dir
+
+def fetch_extras(session, base_url, uuid, version, col_id, latest=False):
+    # Extras (includes head and downloadable file info)
+    extras_url = '{}/extras/{}@{}'.format(base_url, uuid, version)
+    resp = session.get(extras_url)
+
+    # Latest defaults to successfully baked - we need headVersion
+    if latest:
+        version = resp.json()['headVersion']
+        extras_url = '{}/extras/{}@{}'.format(base_url, uuid, version)
+        resp = session.get(extras_url)
+
+    col_extras = resp.json()
+    if version != col_extras['headVersion']:
+        logger.warning(
+            """
+            Fetching non-head version of {}.
+            Head: {},
+            Requested: {}
+            """.format(col_id, col_extras['headVersion'], version))
+        if not(confirm("Fetch anyway? [y/n] ")):
+            raise OldContent()
+
+@click.command()
+@common_params
+@click.option('-d', '--output-dir', type=click.Path(),
+              help="output directory name (can't previously exist)")
+@click.option('-t', '--book-tree', is_flag=True,
+              help="create human-friendly book-tree")
+@click.option('-r', '--with-resources', is_flag=True, default=False,
+              help="Also get all resources (images)")
+@click.argument('env')
+@click.argument('col_id')
+@click.argument('col_version')
+@click.pass_context
+def get(ctx, env, col_id, col_version, output_dir, book_tree, with_resources):
+    """download and expand the completezip to the current working directory"""
+    start_time = time.time()
+    base_url = build_archive_url(ctx, env)
+    req_version, col_version, version = _get_versions(col_version)
+
+    # Create a request session with retries if there's failed DNS lookups,
+    # socket connections and connection timeouts.
+    # See https://stackoverflow.com/questions/33895739/
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=5)
+    session.mount('https://', adapter)
+
+    legacy_url = build_legacy_url(base_url, col_id, col_version)
+    collection_metadata = fetch_metadata(session, base_url, legacy_url, col_id, req_version, version)
+
+    version = collection_metadata['version']
+    output_dir = build_output_directory(output_dir, col_id, version)
+
+    uuid = collection_metadata['id']
+    latest = col_version == 'latest'
+    fetch_extras(session, base_url, uuid, version, col_id, latest)
+
+    # Write tree
+    tree = collection_metadata['tree']
+    os.mkdir(str(output_dir))
+
+    num_pages = _count_nodes(tree) + 1  # Num. of xml files to fetch
+    try:
+        label = 'Getting {}'.format(output_dir.relative_to(Path.cwd()))
+    except ValueError:
+        # Raised ONLY when output_dir is not a childnode of cwd
+        label = 'Getting {}'.format(output_dir)
+    with click.progressbar(length=num_pages,
+                           label=label,
+                           width=0,
+                           show_pos=True) as pbar:
+        _write_node(tree, base_url, output_dir, book_tree, with_resources, pbar)
+
+    duration = time.time() - start_time
+    print("Fetch took {} seconds".format(duration))
