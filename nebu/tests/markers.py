2138f346807ca73e33778965ae1285fb2e56556c
Chris Kline <ckline.tryptic@gmail.com>
2020-09-09 14:43:43 +0000

    move tests from cnx-automation

---
+++ nebu/tests/markers.py
@@ -0,0 +1,31 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from pytest import mark
+from pytest_testrail.plugin import pytestrail
+
+# Markers need to be registered in conftest.py and added as a return value
+# in the get_custom_markers() function.
+# FIXME: Replace all instances of @markers.{custom_marker} with @pytest.mark.{custom_marker}
+
+nondestructive = mark.nondestructive
+parametrize = mark.parametrize
+test_case = pytestrail.case
+xfail = mark.xfail
+vendor = mark.vendor
+
+slow = mark.slow
+smoke = mark.smoke
+
+webview = mark.webview
+legacy = mark.legacy
+neb = mark.neb
+rex = mark.rex
+awss3 = mark.awss3
+archive = mark.archive
+
+requires_complete_dataset = mark.requires_complete_dataset
+requires_deployment = mark.requires_deployment
+requires_publishing = mark.requires_publishing
+requires_varnish_routing = mark.requires_varnish_routing
